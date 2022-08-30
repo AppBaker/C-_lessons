0f2d2d6cf2b5d7fd5d2ce50f96f3c12956126bb6
Ivan Nikitin <qqaqq@ya.ru>
2022-08-30 23:14:35 +0700

    Add Example002 Sum

---
+++ Example002_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int numberA = 34;
+int numberB = 96;
+int result = numberA + numberB;
+Console.WriteLine(result);
