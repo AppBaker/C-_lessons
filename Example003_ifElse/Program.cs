31ed35d09536f4c2a822defedf27cde7ef0dd6cb
Ivan Nikitin <qqaqq@ya.ru>
2022-09-01 10:07:34 +0700

    Add Example003 ifElse

---
+++ Example003_ifElse/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя:");
+string username = Console.ReadLine();
+if (username == "John") {
+    Console.WriteLine("All right, you are " + username);
+} else {
+    Console.WriteLine("You are John");
+}
+
